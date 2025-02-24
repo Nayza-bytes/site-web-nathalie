f626b7f8505cea6800c338fe335218df8358597e
Ilian <guille.ilian.lgm1@gmail.com>
2025-02-24 20:42:32 +0100

    Add source files

---
+++ src/sections/Navbar.js
@@ -0,0 +1,25 @@
+const Navbar = () => {
+    return (
+        <>
+            <div className="">
+                <div className="">
+                    <p>Nathalie</p>
+                </div>
+                <div className="">
+                    <p>Stampfli</p>
+                </div>
+                <div className="">
+                    <ul>
+                        <li>A Propos</li>
+                        <li>Services</li>
+                        <li>Tarifs</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Navbar;
