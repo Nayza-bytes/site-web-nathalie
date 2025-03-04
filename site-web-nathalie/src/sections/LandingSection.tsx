d260b615cd0b1e5aa28722b82d72ebd0d625ed2a
Ilian <guille.ilian.lgm1@gmail.com>
2025-03-04 18:32:34 +0100

    Add the landing section

---
+++ site-web-nathalie/src/sections/LandingSection.tsx
@@ -0,0 +1,11 @@
+import '../index.css';
+
+const LandingSection = () => {
+    return (
+        <section className='h-screen bg-background'>
+
+        </section>
+    );
+}
+
+export default LandingSection;
