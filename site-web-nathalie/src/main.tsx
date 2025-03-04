9a04f4a20af7eadbaceedddd5848fbd577e4193c
Ilian <guille.ilian.lgm1@gmail.com>
2025-03-04 18:33:48 +0100

    Add LandingSection to display

--- site-web-nathalie/src/main.tsx
+++ site-web-nathalie/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Navbar from './sections/Navbar'
+import LandingSection from './sections/LandingSection'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Navbar />
+    <LandingSection />
   </StrictMode>,
 )
