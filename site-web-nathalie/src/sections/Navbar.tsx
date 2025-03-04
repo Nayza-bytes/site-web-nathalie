c1f8f29e8d0f3891ae30c75a806099ff5ae54993
Ilian <guille.ilian.lgm1@gmail.com>
2025-03-04 18:25:29 +0100

    Make the navbar

--- site-web-nathalie/src/sections/Navbar.tsx
+++ site-web-nathalie/src/sections/Navbar.tsx
@@ -2,21 +2,21 @@ import '../index.css';
 
 const Navbar = () => {
     return (
-        <nav className="">
+        <nav className="flex justify-evenly items-center h-20 bg-background">
             <div className='text-xl'>
-                <div className="">
+                <div className="font-logo">
                     <p>Nathalie</p>
                 </div>
-                <div className="">
+                <div className="font-logo ml-14">
                     <p>Stampfli</p>
                 </div>
             </div>
-            <div>
-                <ul className="">
-                    <li><a>A Propos</a></li>
-                    <li><a>Services</a></li>
-                    <li><a>Tarifs</a></li>
-                    <li><a>Contact</a></li>
+            <div >
+                <ul className='flex justify-evenly font-mh-light'>
+                    <li className='p-6'><a href='#'>A Propos</a></li>
+                    <li className='p-6'><a href='#'>Services</a></li>
+                    <li className='p-6'><a href='#'>Tarifs</a></li>
+                    <li className='p-6'><a href='#'>Contact</a></li>
                 </ul>
             </div>
         </nav>
