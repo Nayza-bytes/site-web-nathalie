09254f028d6ea39cfe7a7458047a1f388bf12c01
Ilian <guille.ilian.lgm1@gmail.com>
2025-03-05 18:17:06 +0100

    Start of the landing section

--- site-web-nathalie/src/sections/LandingSection.tsx
+++ site-web-nathalie/src/sections/LandingSection.tsx
@@ -3,7 +3,26 @@ import '../index.css';
 const LandingSection = () => {
     return (
         <section className='h-screen bg-background'>
-
+            <div className='font-logo text-4xl flex flex-col justify-center items-center h-56 mr-16'>
+                <h1>Soulager vos douleurs et</h1>
+                <h1 className='ml-52 mt-4'>retrouvez votre confort</h1>
+            </div>
+            <div>
+                <p>
+                    Grâce au massage holistique, 
+                    retrouver un équilibre entre vos muscles, 
+                    vos sens, vos émotions et votre mental pour plein de bénéfices !
+                </p>
+            </div>
+            <div>
+                <a>Je prends rendez-vous</a>
+            </div>
+            <div>
+                <a>Les tarifs</a>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
         </section>
     );
 }
